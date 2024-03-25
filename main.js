603e48b698de465bdd69842124f449b8b1eebab0
DamianoSpampinato <ghgg585@gmail.com>
2024-03-25 18:41:28 +0100

    feat created function dynamic template +

--- main.js
+++ main.js
@@ -55,3 +55,49 @@ const posts = [
         "created": "2021-03-05"
     }
 ];
+
+//Seleziono il container dei post
+const postContainer = document.querySelector('#container.posts-list');
+//scorro l'array dei post
+posts.forEach((postItem) =>{
+    const postTemplate = generatePostItemTemplate(postItem);
+    postContainer.innerHTML += postTemplate;
+})
+
+//functions
+function generatePostItemTemplate(postItem){
+    const {content, likes, created, author, media , id} = postItem
+    const postTemplate =`
+    <div class="post">
+            <div class="post__header">
+                <div class="post-meta">                    
+                    <div class="post-meta__icon">
+                        <img class="profile-pic" src=${author.image} alt="Phil Mangione">                    
+                    </div>
+                    <div class="post-meta__data">
+                        <div class="post-meta__author">${author.name}</div>
+                        <div class="post-meta__time">4 mesi fa</div>
+                    </div>                    
+                </div>
+            </div>
+            <div class="post__text">Placeat libero ipsa nobis ipsum quibusdam quas harum ut. Distinctio minima iusto. Ad ad maiores et sint voluptate recusandae architecto. Et nihil ullam aut alias.</div>
+            <div class="post__image">
+                <img src=${media} alt="">
+            </div>
+            <div class="post__footer">
+                <div class="likes js-likes">
+                    <div class="likes__cta">
+                        <a class="like-button  js-like-button" href="#" data-postid="1">
+                            <i class="like-button__icon fas fa-thumbs-up" aria-hidden="true"></i>
+                            <span class="like-button__label">Mi Piace</span>
+                        </a>
+                    </div>
+                    <div class="likes__counter">
+                        Piace a <b id="like-counter-1" class="js-likes-counter">${likes}</b> persone
+                    </div>
+                </div> 
+            </div>            
+        </div>
+    `
+    return postTemplate
+}
